577e3ade268ab2ad7e08fdaecd0ece56fb3eb25d
frsv <fursovroman95@gmail.com>
2017-02-26 22:54:49 +0300

    Updated additional rules, fix syntax error

--- tests/rules/snake-case.js
+++ tests/rules/snake-case.js
@@ -6,11 +6,22 @@ module.exports = {
         'var foo_bar = 1',
         'var foo_bar_baz = 1',
         { code: 'var FOO_BAR_BAZ = 1', options: [{ allowCapitalized: true }] },
-        { code: 'class fooBarBaz', options: [{ capitalizedClasses: true }] },
+        { code: 'class fooBarBaz {}', options: [{ camelcasedClasses: true }] },
     ],
     invalid: [
-        {code: 'var fooBar = 1', errors: [{message: "Unexpected var declaration with 'üœø' name."}]},
-        {code: 'var foo_Bar = 2', errors: [{message: "Unexpected let declaration with 'ёж' name."}]},
-        {code: 'class foo_bar_baz', errors: [{message: "Unexpected const declaration with '火' name."}]}
+        {
+            code: 'var fooBar = 1',
+            errors: [{message: "Identifier 'fooBar' is not in snake case."}],
+        },
+        {
+            code: 'var FOO_BAR = 2',
+            options: [{ allowCapitalized: false }],
+            errors: [{message: "Identifier 'FOO_BAR' is not in snake case."}],
+        },
+        {
+            code: 'class foo_bar_baz',
+            options: [{ camelcasedClasses: true }],
+            errors: [{message: "Identifier 'foo_bar_baz' is not in snake case."}],
+        }
     ]
 };
